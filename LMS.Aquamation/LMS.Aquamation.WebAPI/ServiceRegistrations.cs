2f48ccf58f0a2c960d3770498fa6ae3e38261ebb
Hamid Molareza <molareza@protonmail.com>
2025-01-15 11:11:11 +0330

    chore(gRPC): config it

--- LMS.Aquamation/LMS.Aquamation.WebAPI/ServiceRegistrations.cs
+++ LMS.Aquamation/LMS.Aquamation.WebAPI/ServiceRegistrations.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using LMS.Aquamation.Application;
 using LMS.Aquamation.Core;
 using LMS.Aquamation.Infrastructure;
@@ -9,5 +10,19 @@ public static class ServiceRegistrations {
         ApplicationServiceRegistration.AddServices(service);
         InfrastructureServiceRegistration.AddServices(service, sqlConnectionString);
         DomainServiceRegistration.AddServices(service);
+        AddWebApiServices(service);
+    }
+
+    public static void AddWebApiServices(IServiceCollection services) {
+        #region Grpc Client Config
+
+        services.AddGrpc(options => {
+            options.ResponseCompressionLevel = CompressionLevel.Optimal;
+            options.ResponseCompressionAlgorithm = "gzip";
+            options.MaxSendMessageSize = 1024 * 1024 * 1024; // 1 GB
+            options.MaxReceiveMessageSize = 1024 * 1024 * 1024; // 1 GB
+        });
+
+        #endregion
     }
 }
