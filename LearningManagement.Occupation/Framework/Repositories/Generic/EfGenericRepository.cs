02125c43de843e5bde6d0ce27af8dfdf90ca76e0
Hamid Molareza <molareza@protonmail.com>
2025-01-18 10:29:56 +0330

    fix(Repository): improve generic repository

--- LearningManagement.Occupation/Framework/Repositories/Generic/EfGenericRepository.cs
+++ LearningManagement.Occupation/Framework/Repositories/Generic/EfGenericRepository.cs
@@ -4,11 +4,13 @@ using Microsoft.EntityFrameworkCore;
 namespace Framework.Repositories.Generic;
 
 public class EfGenericRepository<TEntity, TId>(DbContext context)
-    where TEntity : class {
+    where TEntity : BaseEntity {
     protected readonly DbSet<TEntity> DbSet = context.Set<TEntity>();
+    
+    private IQueryable<TEntity> GetAsNoTrackingDbSet(bool asNoTracking) => asNoTracking ? DbSet.AsNoTracking() : DbSet.AsQueryable();
 
     public Task<List<TEntity>> GetAllAsync(bool asNoTracking = true, CancellationToken cancellationToken = default) {
-        var query = asNoTracking ? DbSet.AsNoTracking() : DbSet.AsQueryable();
+        var query = GetAsNoTrackingDbSet(asNoTracking);
         return query.ToListAsync(cancellationToken);
     }
 
@@ -20,12 +22,44 @@ public class EfGenericRepository<TEntity, TId>(DbContext context)
         return entity;
     }
 
+    public async Task<TEntity?> GetByIdAsync(TId id, bool asNoTracking = true, CancellationToken cancellationToken = default) {
+        var entity = await DbSet.FindAsync([id], cancellationToken);
+        if (entity == null || !asNoTracking) return entity;
+
+        context.Entry(entity).State = EntityState.Detached;
+        return entity;
+    }
+
+    public TEntity? GetById(TId id, bool asNoTracking = true) {
+        var entity = DbSet.Find(id);
+        if (entity == null || !asNoTracking) return entity;
+
+        context.Entry(entity).State = EntityState.Detached;
+        return entity;
+    }
+
+    public async Task<bool> ExistsAsync(TId id, CancellationToken cancellationToken = default) {
+        return await DbSet.FindAsync([id], cancellationToken) != null;
+    }
+
+    public bool Exists(TId id) {
+        return DbSet.Find(id) != null;
+    }
+
     public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default) {
         await DbSet.AddAsync(entity, cancellationToken: cancellationToken);
     }
 
+    public void Add(TEntity entity) {
+        DbSet.Add(entity);
+    }
+
     public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default) {
         return DbSet.AddRangeAsync(entities, cancellationToken: cancellationToken);
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities) {
+        DbSet.AddRange(entities);
     }
 
     public void Update(TEntity entity) {
@@ -48,8 +82,11 @@ public class EfGenericRepository<TEntity, TId>(DbContext context)
         }
     }
 
-
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) {
         return context.SaveChangesAsync(cancellationToken: cancellationToken);
+    }
+
+    public int SaveChanges() {
+        return context.SaveChanges();
     }
 }
--- LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Companies/CompanyRepository.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Companies/CompanyRepository.cs
@@ -1,20 +1,8 @@
 using Framework.Repositories.Generic;
 using LearningManagement.Occupation.Domain.Companies;
 using LearningManagement.Occupation.Domain.Companies.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace LearningManagement.Aquamation.Infrastructure.Companies;
 
 public class CompanyRepository(AppDbContext context)
-    : EfGenericRepository<Company, long>(context), ICompanyRepository {
-    public Task<Company?> GetByIdAsync(long id, bool asNoTracking = true, CancellationToken cancellationToken = default) {
-        var queryable = asNoTracking ? DbSet.AsNoTracking() : DbSet;
-        return queryable.FirstOrDefaultAsync(item => item.Id == id, cancellationToken);
-    }
-
-    public void Add(Company company) {
-        DbSet.Add(company);
-    }
-
-    public Task<bool> ExistsAsync(long id, CancellationToken cancellationToken = default) => DbSet.AnyAsync(item => item.Id == id, cancellationToken);
-}
+    : EfGenericRepository<Company, long>(context), ICompanyRepository;
