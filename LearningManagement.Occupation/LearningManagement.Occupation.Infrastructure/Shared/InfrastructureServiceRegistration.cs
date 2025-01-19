aed92298a1f3ef5cd56c67d5d2fb7067ec7c74e6
Hamid Molareza <molareza@protonmail.com>
2025-01-19 08:53:11 +0330

    refactor: `InfrastructureServiceRegistration`

--- LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Shared/InfrastructureServiceRegistration.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Shared/InfrastructureServiceRegistration.cs
@@ -9,19 +9,29 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace LearningManagement.Occupation.Infrastructure.Shared;
 
-//TODO: Refactor
 public static class InfrastructureServiceRegistration {
     public static void AddServices(IServiceCollection service, string sqlServerConnectionString) {
-        service.AddDbContext<AppDbContext>((serviceProvider, options) => {
+        AddDatabase(service, sqlServerConnectionString);
+        AddRepositories(service);
+        AddInterceptors(service);
+        AddMapper(service);
+    }
+
+    private static void AddDatabase(IServiceCollection service, string sqlServerConnectionString) => service.AddDbContext<AppDbContext>(
+        (serviceProvider, options) => {
             options.UseSqlServer(sqlServerConnectionString)
                 .AddInterceptors(serviceProvider.GetRequiredService<SoftDeleteInterceptor>())
                 .AddInterceptors(serviceProvider.GetRequiredService<AuditabilityInterceptor>());
         });
 
-        service.AddScoped<ICompanyRepository, EfCompanyRepository>();
+    private static void AddRepositories(IServiceCollection service) => service.AddScoped<ICompanyRepository, EfCompanyRepository>();
+
+    private static void AddInterceptors(IServiceCollection service) {
         service.AddScoped<SoftDeleteInterceptor>();
         service.AddScoped<AuditabilityInterceptor>();
+    }
 
+    private static void AddMapper(IServiceCollection service) {
         service.AddSingleton<IMapper, Mapper>();
         var typeAdapterConfig = TypeAdapterConfig.GlobalSettings;
         typeAdapterConfig.Scan(typeof(InfrastructureServiceRegistration).Assembly);
