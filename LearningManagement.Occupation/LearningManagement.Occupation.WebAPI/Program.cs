b7d0542a0fe7ab695b92b7b4a7fdcddbf3a981a4
Hamid Molareza <molareza@protonmail.com>
2025-01-19 14:20:41 +0330

    chore: change keyname of connection string to `Occupation`

--- LearningManagement.Occupation/LearningManagement.Occupation.WebAPI/Program.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.WebAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-var connectionString = builder.Configuration.GetConnectionString("Default")
+var connectionString = builder.Configuration.GetConnectionString("Occupation")
                        ?? throw new ArgumentException("Can not find database connection string.");
 ServiceRegistrations.AddServices(builder.Services, connectionString);
 
