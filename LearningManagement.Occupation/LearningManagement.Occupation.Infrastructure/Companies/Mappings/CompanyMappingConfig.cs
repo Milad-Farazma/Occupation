235b7c9818ae75ca284edae892e601dec5c320f3
Hamid Molareza <molareza@protonmail.com>
2025-01-20 11:42:57 +0330

    chore: minor updates

--- LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Companies/Mappings/CompanyMappingConfig.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Companies/Mappings/CompanyMappingConfig.cs
@@ -5,15 +5,6 @@ namespace LearningManagement.Occupation.Infrastructure.Companies.Mappings;
 
 public class CompanyMappingConfig : IRegister {
     public void Register(TypeAdapterConfig config) {
-        TypeAdapterConfig<CreateCompanyRequest, Company>.NewConfig()
-            .Map(dest => dest.Title, src => src.Title)
-            .Map(dest => dest.Description, src => src.Description)
-            .Map(dest => dest.LogoUrl, src => src.LogoUrl);
-
-        TypeAdapterConfig<Company, CompanyDto>.NewConfig()
-            .Map(dest => dest.Title, src => src.Title)
-            .Map(dest => dest.IsApproved, src => src.IsApproved);
-
         TypeAdapterConfig<UpdateCompanyRequest, Company>.NewConfig()
             .Map(dest => dest.Title, src => src.NewTitle);
     }
