ac1db897ba67885d0129c2e72f369b1bcc3aca6b
Hamid Molareza <molareza@protonmail.com>
2025-01-20 14:14:29 +0330

    chore: minor updates

--- LearningManagement.Occupation/Framework/Services/User/UserService.cs
+++ LearningManagement.Occupation/Framework/Services/User/UserService.cs
@@ -8,9 +8,9 @@ public sealed class UserService(
     IHttpContextAccessor httpContextAccessor,
     ILogger<UserService> logger) : IUserService {
     public long GetCurrentUserId() {
-        ClaimsPrincipal? user = httpContextAccessor.HttpContext?.User;
+        var user = httpContextAccessor.HttpContext?.User;
 
-        bool? isAuthenticated = user?.Identity?.IsAuthenticated;
+        var isAuthenticated = user?.Identity?.IsAuthenticated;
 
         if (!isAuthenticated ?? false) {
             logger.LogWarning("Trying to get current userId for an unauthenticated user!, " +
@@ -18,9 +18,9 @@ public sealed class UserService(
             return 0;
         }
 
-        string? userIdClaim = user?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var userIdClaim = user?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-        if (string.IsNullOrWhiteSpace(userIdClaim) || !long.TryParse(userIdClaim, out long userId)) {
+        if (string.IsNullOrWhiteSpace(userIdClaim) || !long.TryParse(userIdClaim, out var userId)) {
             logger.LogWarning("Trying to get current userId for an user without userId claim!, " +
                               "HttpContextRequest: {Request}", httpContextAccessor.HttpContext?.Request);
             return 0;
