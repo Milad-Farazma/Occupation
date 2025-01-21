cd77693837f86b6f32477fbdffb230c964d1fb6a
Hamid Molareza <molareza@protonmail.com>
2025-01-21 10:43:13 +0330

    fix: config auto-increment for database

--- LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Departments/EntityFramework/Configurations/DepartmentConfiguration.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Departments/EntityFramework/Configurations/DepartmentConfiguration.cs
@@ -7,23 +7,26 @@ namespace LearningManagement.Occupation.Infrastructure.Departments.EntityFramewo
 public class DepartmentConfiguration : IEntityTypeConfiguration<Department> {
     public void Configure(EntityTypeBuilder<Department> builder) {
         builder.ToTable("Departments");
-        builder.HasKey(b => b.Id);
-        builder.OwnsOne(b => b.AuditInfo, nb => {
-            nb.Property(c => c.CreatedByUserId).HasColumnName(WellKnownNames.AuditInfo.CreatedByUserId);
-            nb.Property(c => c.ModifiedByUserId).HasColumnName(WellKnownNames.AuditInfo.ModifiedByUserId);
-            nb.Property(c => c.CreatedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.CreatedAtUtcDateTime);
-            nb.Property(c => c.ModifiedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.ModifiedAtUtcDateTime);
+
+        builder.HasKey(entity => entity.Id);
+        builder.Property(entity => entity.Id).ValueGeneratedOnAdd();
+
+        builder.OwnsOne(entity => entity.AuditInfo, nb => {
+            nb.Property(auditInfo => auditInfo.CreatedByUserId).HasColumnName(WellKnownNames.AuditInfo.CreatedByUserId);
+            nb.Property(auditInfo => auditInfo.ModifiedByUserId).HasColumnName(WellKnownNames.AuditInfo.ModifiedByUserId);
+            nb.Property(auditInfo => auditInfo.CreatedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.CreatedAtUtcDateTime);
+            nb.Property(auditInfo => auditInfo.ModifiedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.ModifiedAtUtcDateTime);
         });
-        builder.OwnsOne(b => b.SoftDeleteInfo, nb => {
-            nb.Property(c => c.IsDeleted).HasColumnName(WellKnownNames.SoftDeleteInfo.IsDeleted);
-            nb.Property(c => c.DeletedAtUtcDateTime).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedAtUtcDateTime);
-            nb.Property(c => c.DeletedByUserId).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedByUserId);
+        builder.OwnsOne(entity => entity.SoftDeleteInfo, nb => {
+            nb.Property(softDeleteInfo => softDeleteInfo.IsDeleted).HasColumnName(WellKnownNames.SoftDeleteInfo.IsDeleted);
+            nb.Property(softDeleteInfo => softDeleteInfo.DeletedAtUtcDateTime).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedAtUtcDateTime);
+            nb.Property(softDeleteInfo => softDeleteInfo.DeletedByUserId).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedByUserId);
         });
 
-        builder.HasKey(c => c.Id);
-        builder.Property(c => c.Id)
+        builder.HasKey(entity => entity.Id);
+        builder.Property(entity => entity.Id)
             .ValueGeneratedOnAdd(); // Configure auto-increment behavior
 
-        builder.Property(c => c.Title).IsRequired().HasMaxLength(200);
+        builder.Property(entity => entity.Title).IsRequired().HasMaxLength(200);
     }
 }
--- LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Organizations/EntityFramework/Configurations/OrganizationConfiguration.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Organizations/EntityFramework/Configurations/OrganizationConfiguration.cs
@@ -7,30 +7,33 @@ namespace LearningManagement.Occupation.Infrastructure.Organizations.EntityFrame
 public class OrganizationConfiguration : IEntityTypeConfiguration<Organization> {
     public void Configure(EntityTypeBuilder<Organization> builder) {
         builder.ToTable("Organizations");
-        builder.HasKey(b => b.Id);
-        builder.OwnsOne(b => b.AuditInfo, nb => {
-            nb.Property(c => c.CreatedByUserId).HasColumnName(WellKnownNames.AuditInfo.CreatedByUserId);
-            nb.Property(c => c.ModifiedByUserId).HasColumnName(WellKnownNames.AuditInfo.ModifiedByUserId);
-            nb.Property(c => c.CreatedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.CreatedAtUtcDateTime);
-            nb.Property(c => c.ModifiedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.ModifiedAtUtcDateTime);
+
+        builder.HasKey(entity => entity.Id);
+        builder.Property(entity => entity.Id).ValueGeneratedOnAdd();
+
+        builder.OwnsOne(entity => entity.AuditInfo, nb => {
+            nb.Property(auditInfo => auditInfo.CreatedByUserId).HasColumnName(WellKnownNames.AuditInfo.CreatedByUserId);
+            nb.Property(auditInfo => auditInfo.ModifiedByUserId).HasColumnName(WellKnownNames.AuditInfo.ModifiedByUserId);
+            nb.Property(auditInfo => auditInfo.CreatedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.CreatedAtUtcDateTime);
+            nb.Property(auditInfo => auditInfo.ModifiedAtUtcDateTime).HasColumnName(WellKnownNames.AuditInfo.ModifiedAtUtcDateTime);
         });
-        builder.OwnsOne(b => b.SoftDeleteInfo, nb => {
-            nb.Property(c => c.IsDeleted).HasColumnName(WellKnownNames.SoftDeleteInfo.IsDeleted);
-            nb.Property(c => c.DeletedAtUtcDateTime).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedAtUtcDateTime);
-            nb.Property(c => c.DeletedByUserId).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedByUserId);
+        builder.OwnsOne(entity => entity.SoftDeleteInfo, nb => {
+            nb.Property(softDeleteInfo => softDeleteInfo.IsDeleted).HasColumnName(WellKnownNames.SoftDeleteInfo.IsDeleted);
+            nb.Property(softDeleteInfo => softDeleteInfo.DeletedAtUtcDateTime).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedAtUtcDateTime);
+            nb.Property(softDeleteInfo => softDeleteInfo.DeletedByUserId).HasColumnName(WellKnownNames.SoftDeleteInfo.DeletedByUserId);
         });
 
-        builder.HasKey(c => c.Id);
-        builder.Property(c => c.Id)
+        builder.HasKey(entity => entity.Id);
+        builder.Property(entity => entity.Id)
             .ValueGeneratedOnAdd(); // Configure auto-increment behavior
 
-        builder.Property(c => c.Title).IsRequired().HasMaxLength(200);
-        builder.Property(c => c.IsViewable);
-        builder.Property(c => c.IsApproved);
+        builder.Property(entity => entity.Title).IsRequired().HasMaxLength(200);
+        builder.Property(entity => entity.IsViewable);
+        builder.Property(entity => entity.IsApproved);
 
-        builder.HasMany(e => e.Department)
-            .WithOne(p => p.Organization)
-            .HasForeignKey(p => p.OrganizationId)
+        builder.HasMany(entity => entity.Department)
+            .WithOne(entity => entity.Organization)
+            .HasForeignKey(entity => entity.OrganizationId)
             .IsRequired();
     }
 }
