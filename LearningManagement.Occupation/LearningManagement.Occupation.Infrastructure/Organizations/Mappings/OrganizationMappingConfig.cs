0389d82b28b7cc569a916a49f642280ddfbcae1b
Hamid Molareza <molareza@protonmail.com>
2025-01-22 11:17:47 +0330

    fix(organization): mapping department dto

--- LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Organizations/Mappings/OrganizationMappingConfig.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Infrastructure/Organizations/Mappings/OrganizationMappingConfig.cs
@@ -1,6 +1,11 @@
+using LearningManagement.Occupation.Application.Organizations.Dto;
+using LearningManagement.Occupation.Domain.Organizations.Models;
+
 namespace LearningManagement.Occupation.Infrastructure.Organizations.Mappings;
 
 public class OrganizationMappingConfig : IRegister {
     public void Register(TypeAdapterConfig config) {
+        TypeAdapterConfig<Organization, OrganizationDto>.NewConfig()
+            .Map(dest => dest.Departments, src => src.Department);
     }
 }
