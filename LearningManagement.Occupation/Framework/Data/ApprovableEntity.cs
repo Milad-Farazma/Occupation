d33b7759ff981885e525251da870997f8e754a15
Hamid Molareza <molareza@protonmail.com>
2025-01-22 14:04:48 +0330

    chore: format codes

--- LearningManagement.Occupation/Framework/Data/ApprovableEntity.cs
+++ LearningManagement.Occupation/Framework/Data/ApprovableEntity.cs
@@ -1,5 +1,5 @@
 namespace Framework.Data;
 
-public abstract class ApprovableEntity: BaseAuditableAndSoftDeletableEntity {
+public abstract class ApprovableEntity : BaseAuditableAndSoftDeletableEntity {
     public bool IsActive { get; set; }
 }
