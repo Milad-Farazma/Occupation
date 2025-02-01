2ed91cbb0e15baf2f2e7b9c6990d1298ed975f25
Hamid Molareza <molareza@protonmail.com>
2025-02-01 13:40:45 +0330

    chore: format codes

--- LearningManagement.Occupation/LearningManagement.Occupation.Application/Departments/Contracts/IDepartmentRepository.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.Application/Departments/Contracts/IDepartmentRepository.cs
@@ -4,7 +4,8 @@ using LearningManagement.Occupation.Domain.Departments;
 namespace LearningManagement.Occupation.Application.Departments.Contracts;
 
 public interface IDepartmentRepository {
-    Task<PaginatedResult<Department>> GetAllAsync(bool asNoTracking, PaginationRequest request, DepartmentSearchRequest? searchRequest, bool loadRelations, CancellationToken cancellationToken = default);
+    Task<PaginatedResult<Department>> GetAllAsync(bool asNoTracking, PaginationRequest request, DepartmentSearchRequest? searchRequest,
+        bool loadRelations, CancellationToken cancellationToken = default);
 
     Task<Department?> GetByIdAsync(long id, bool asNoTracking, bool loadRelations, CancellationToken cancellationToken = default);
     void Add(Department entity);
