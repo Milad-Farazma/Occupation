0e7d4585447be55e6683136c099c2c37c426d350
Hamid Molareza <molareza@protonmail.com>
2025-02-05 13:38:07 +0330

    fix: apply auto migration in development

--- LearningManagement.Occupation/LearningManagement.Occupation.WebAPI/Program.cs
+++ LearningManagement.Occupation/LearningManagement.Occupation.WebAPI/Program.cs
@@ -1,7 +1,9 @@
 using LearningManagement.Occupation.Application.Shared.Extensions;
+using LearningManagement.Occupation.Infrastructure.Shared;
 using LearningManagement.Occupation.Infrastructure.Shared.Extensions;
 using LearningManagement.Occupation.WebAPI.Middlewares;
 using LearningManagement.Occupation.WebAPI.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +23,13 @@ builder.Host.UseSerilog((context, configuration) =>
 );
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+    // Apply migrations
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
 
 app.UseSerilogRequestLogging();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
